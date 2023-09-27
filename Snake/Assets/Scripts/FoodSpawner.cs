f0544327fd9860ffc881976ec1fc5d3ce6eea80b
didenkokirill <94925492+didenkokirill@users.noreply.github.com>
2023-09-27 13:48:51 +0300

    Basic mechanics (#1)
    * add basic mechanics
    
    * fix code stile
    
    * clear code

---
+++ Snake/Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    [SerializeField] private float spawnDelay;
+    [SerializeField] private Vector2 spawnArea;
+
+    [SerializeField] private GameObject applePrefab;
+
+    [SerializeField] private GameObject currentFood;
+
+    private void Update()
+    {
+        if (currentFood == null)
+        {
+            SpawnFood(DefinitionPositin());
+        }
+    }
+
+    private Vector3 DefinitionPositin()
+    {
+        Vector3 position = new Vector3(
+            Mathf.Round(Random.Range(-spawnArea.x, spawnArea.x)),
+            Mathf.Round(Random.Range(-spawnArea.y, spawnArea.y)),
+            0f);
+
+        return (position);
+    }
+
+    private void SpawnFood(Vector3 pos)
+    {
+        GameObject food = Instantiate(applePrefab, pos, Quaternion.identity);
+
+        currentFood = food;
+    }
+}
